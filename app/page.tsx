a0011fa0c3d145d7044f459012907f40ad40ab9a
julian <julilito09@gmail.com>
2024-10-30 08:27:44 -0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,23 @@
-import Image from "next/image";
-
+import ImgWomen from "../public/women.jpg"
+import Image from "next/image"
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="bg-gradient-to-tr from-cyan-300 to-blue-900 h-screen w-screen items-center justify-center flex">
+      <section className="bg-slate-950 lg:h-[60%] lg:w-[50%] rounded-3xl ">
+        <dl className="rounded-3xl flex gap-10 sm:flex-col lg:flex-row">
+          <Image className=" order-1 rounded-full sm:rounded-3xl lg:rounded-3xl
+          h-[200px] w-[200px] sm:h-[250px] sm:w-[250px] sm:justify-center lg:my-10 lg:-ml-10 lg:h-[280px] lg:w-[250px]  object-cover lg:object-cover shadow-2xl shadow-black" src={ImgWomen} alt="" />
+          <dl className=" order-2 lg:w-[70%] text-left flex flex-col justify-center">
+            <h1 className="text-2xl text-white font-serif lg:text-left ">Jess Wilson</h1>
+            <p className="font-serif text-slate-50 text-opacity-60">UX Engineer</p>
+            <p className=" font-serif text-sm text-slate-50 text-opacity-40">Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.</p>
+            <ul className="mt-10 flex justify-left gap-12">
+              <button className="font-serif text-center border rounded-3xl w-[30%] h-[130%] border-slate-50">Profile</button>
+              <button className="font-serif text-center text-slate-950 border rounded-3xl w-[30%] h-[130%] bg-slate-50">Follow</button>
+            </ul>
+          </dl>
+        </dl>
+      </section>
+    </main>
   );
 }
